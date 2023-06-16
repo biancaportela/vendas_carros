f67af8009b626e7a99ee728c80024ef1a614b0e3
biancaportela <biancaportelaca@gmail.com>
2023-06-15 21:37:58 -0300

    Update home.py

--- app/home.py
+++ app/home.py
@@ -19,22 +19,20 @@ output = "sales_rfr.pkl"
 # Fazer o download do arquivo do Google Drive
 gdown.download(url, output, quiet=False)
 
-directory = '/app/vendas_carros/'
+# Diretório onde o arquivo sales_rfr.pkl é salvo
+directory = 'vendas_carros/app/'
 
 # Listar os arquivos no diretório
 files = os.listdir(directory)
 
-# Verificar se o arquivo "sales_rfr.pkl" está presente
+# Verificar se o arquivo sales_rfr.pkl está presente
 if 'sales_rfr.pkl' in files:
     print("O arquivo 'sales_rfr.pkl' está presente no diretório.")
 else:
     print("O arquivo 'sales_rfr.pkl' não foi encontrado no diretório.")
-
-# Verificar o diretório de trabalho
-print("Diretório de trabalho:", os.getcwd())
-
-# Verificar se o arquivo está presente
-print("Arquivos no diretório:", os.listdir())
+    
+    
+    
 # Carregar o modelo treinado
 with open(output, 'rb') as file:
     model = pickle.load(file)
